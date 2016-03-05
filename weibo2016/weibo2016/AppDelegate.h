25830b97a622a897975c147baf036faf74bcb4f4
slm1949 <slm1949@163.com>
2016-03-05 11:55:17 +0800

    初始化项目

---
+++ weibo2016/weibo2016/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo2016
+//
+//  Created by mac on 16/3/5.
+//  Copyright © 2016年 songlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
